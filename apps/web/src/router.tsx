67b6ba0a307b3651ef697a0064e02448aa307e04
kennycha <dudqn136@naver.com>
2025-07-15 23:00:28 +0900

    fix: change to hash router

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import ProtectedRoute from "./components/protected-route";
 import HomePage from "./pages/home-page";
 import LoginPage from "./pages/login-page";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: (
