075680956768a6fea8b67a5e759f8df626d31f30
kennycha <dudqn136@naver.com>
2025-07-15 22:46:15 +0900

    chore: update fetch duration

--- apps/worker/src/main.rs
+++ apps/worker/src/main.rs
@@ -68,7 +68,7 @@ async fn main() -> Result<()> {
                 }
             }
             None => {
-                sleep(Duration::from_secs(5)).await;
+                sleep(Duration::from_secs(60)).await;
             }
         }
     }
